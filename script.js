1568d8cc150e4b4959762927bd793f88d66837ed
Rajnish Kumar <rajnish.25b01010303@abes.ac.in>
2026-02-23 16:28:11 +0530

    js

---
+++ script.js
@@ -0,0 +1,104 @@
+// console.log("Hello, World!");
+
+
+// var a = 10;
+// var b = 20;
+// var sum = a + b;
+// console.log("Sum of a and b is:", sum);
+
+
+// var a;
+// console.log("Value of a is:", a);
+
+// console.log("let introduction");
+// let a = 10;
+// // let a = 30;
+// console.log("Value of a is:", a);
+
+// const x = 100;
+// const x = 200; 
+// console.log("Value of x is:", x);
+
+
+// Data Types in JavaScript (7 primitive data types)
+// Number
+// NULL
+// SYMBOL
+// STRING
+// BOOLEAN
+// BIGINT
+// UNDEFINED
+
+// let a = "string";
+// let b = 1234;
+// let c = bigint(1234);
+// let d = Symbol;
+// let e = true/false;
+// let d = undefined;
+// let e = null;
+
+// console.log(type of a);
+
+// const obj = {
+//     key: "value",
+//     Name : "Ram",
+//     Branch : "CSE",
+//     Age : 19,
+//     City : "Varanasi",
+//     "Roll No" : 1234,
+    
+// }
+// console.log(obj);
+
+
+//Operators in JavaScript
+// Arithmetic Operators
+
+// let a = 10;
+// let b = 20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+
+// Assignment Operators
+// let c = 10;
+// c += 5; 
+// console.log(c);
+// c-= 3;
+// console.log(c);
+// c*= 2;
+// console.log(c);
+// c/= 4;
+// console.log(c);
+// c%= 3;
+// console.log(c);
+// c**= 2;
+// console.log(c);
+
+
+
+// Comparison Operators : == (value) (loose equality) and === (strict equality) (type) 
+// >, <, >=, <=, !=, !== 
+
+let x = 10;
+let y = "10";
+console.log(x == y); 
+// Logical Operators
+let a = 10;
+let b = 20;
+console.log(a > 5 && b < 30);
+console.log(a > 15 || b < 30);
+//string concatenation
+let str1 = "Hello, ";
+let str2 = "World!";
+console.log(str1 + str2);
+
+// Ternary Operator
+// condition? true : false;
+let age = 18;
+let canVote = age >= 18 ? "Yes, you can vote." : "No, you cannot vote.";
+console.log(canVote);
